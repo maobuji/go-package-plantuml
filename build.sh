d1db861c987124d7c62993fcde87598b7836f767
wwwido <wwwido@qq.com>
2018-11-02 10:43:13 +0800

    修改

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 go build -o $GOPATH/bin/pkgviewer main.go
 
+p=./plantuml.jar
+if [ ! -f $p ];then
+wget -O $p https://nchc.dl.sourceforge.net/project/plantuml/plantuml.jar
+fi
+
+
 p=$GOPATH/third/plantuml.jar
 
 if [ ! -f $p ];then
